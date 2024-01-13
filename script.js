a7d712c928a22d727ba4c9bd47773062e791afc4
mmiriamre <156525278+mmiriamre@users.noreply.github.com>
2024-01-13 16:37:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yesssssss, see you on the 13th of January at any time you want!";
   gif.src =
-    "<div class="tenor-gif-embed" data-postid="13937626585479535666" data-share-method="host" data-aspect-ratio="1.66" data-width="100%"><a href="https://tenor.com/view/fruitchive-eliosgifs-fruitchivegif-gif-13937626585479535666">Fruitchive Eliosgifs GIF</a>from <a href="https://tenor.com/search/fruitchive-gifs">Fruitchive GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>";
+    "<div class="https://media.giphy.com/media/sIV0wFDrsKNxe/giphy.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
